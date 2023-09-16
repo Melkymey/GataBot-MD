a9548ff720f922cb59e4e538edffd475de8d7c51
meikelop <142100454+Melkymey@users.noreply.github.com>
2023-09-17 00:50:23 +0800

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -10,11 +10,11 @@ if (m.quoted.id == this.tebaklagu[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
 if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
-m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} XP`)
+m.reply(`✅Benar!\n+${this.tebaklagu[id][2]} XP`)
 clearTimeout(this.tebaklagu[id][3])
 delete this.tebaklagu[id]
 } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`)
-else m.reply(`❌Incorrecto!`)
+else m.reply(`❌Salah!`)
 }
 return !0
 },
